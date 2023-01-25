4f8fad15f8ef2f356ea3db3f907749e8c2bd0a11
Vladyslav Ksendziuk <vlad.borland13@gmail.com>
2023-01-24 18:32:05 -0600

    added SoftAssert understanding class

--- src/test/java/scripts/TechGlobalBase.java
+++ src/test/java/scripts/TechGlobalBase.java
@@ -3,12 +3,14 @@ package scripts;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.asserts.SoftAssert;
 import pages.*;
 import utilities.ConfigReader;
 import utilities.Driver;
 
 public class TechGlobalBase {
     WebDriver driver;
+    SoftAssert softAssert;
     TechGlobalBasePage techGlobalBasePage;
     TechGlobalFrontendTestingHomePage techGlobalFrontendTestingHomePage;
     TechGlobalDynamicElementsPage techGlobalDynamicElementsPage;
@@ -25,6 +27,7 @@ public class TechGlobalBase {
     public void setup() {
         driver = Driver.getDriver();
 
+        softAssert = new SoftAssert();
         driver.get(ConfigReader.getProperty("appURL"));
         techGlobalBasePage = new TechGlobalBasePage();
     }
@@ -32,6 +35,7 @@ public class TechGlobalBase {
     @AfterMethod
     public void teardown() {
         //We will quit driver and do other proper clean ups
+        softAssert.assertAll();
         Driver.quitDriver();
     }
 }
