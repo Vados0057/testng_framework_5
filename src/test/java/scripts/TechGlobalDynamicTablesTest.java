e71a1af68f7b6e8bd68f0c171e7c3edfa6003dd7
Vladyslav Ksendziuk <vlad.borland13@gmail.com>
2023-01-29 11:29:53 -0600

    StaticTable classes added

--- src/test/java/scripts/TechGlobalDynamicTablesTest.java
+++ src/test/java/scripts/TechGlobalDynamicTablesTest.java
@@ -31,4 +31,22 @@ public class TechGlobalDynamicTablesTest extends TechGlobalBase {
         techGlobalDynamicTablesPage.addProductButton.click();
         Assert.assertTrue(techGlobalDynamicTablesPage.modalCard.isDisplayed());
     }
+
+            /*
+            Go to https://techglobal-training.netlify.app/
+        Click on “Practices” dropdown in the header
+        Select the “Frontend Testing” option
+        Click on the “Dynamic Tables” card
+        Click the “Add Product” button
+        Enter “2” in the Quantity field
+        Enter “Apple Watch” in the Product field
+        Enter “500” in the Price $ field
+        Click the “Submit” button
+        Verify that a new row has been added to the table with the values “2”,
+        “Apple Watch, “500” and the calculated Total $ value is “1,000"
+        Verify that the Total $ value at the bottom of the table has been updated
+        to reflect the total cost of all the products in the table, including the newly added one.
+             */
+
+
 }
